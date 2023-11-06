6361ee7bdc02ff31be1200d12a4aa195dcf15c92
John Matthew <john@compunique.com>
2023-11-05 18:01:37 -0800

    ingest improvements

--- ingest/models.py
+++ ingest/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Rawcsv(models.Model):
+    CSV_TYPE = [
+        ("SC", "Schwab"),
+        ("TD", "TD Ameritrade"),
+    ]
+
     name = models.CharField(max_length=30, blank=True)
     csvfile = models.TextField()
     ingested = models.BooleanField()
+    type = models.CharField(
+        max_length=2,
+        choices=CSV_TYPE, 
+        default="TD",
+    )
 
     def __str__(self) -> str:
-        return f'{self.name} - {self.ingested}'
+        return f'{self.name} - {self.ingested} - {self.type}'
+
 
 
 class Rawtransactions(models.Model):
